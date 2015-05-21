d933ca805fc1615369e0ce24548ddc451db40fd7
jaumard <ioquoo3H>
2015-05-21 12:35:46 +0200

    Fix postinstall script

--- templates/hook/scripts/postinstall.js
+++ templates/hook/scripts/postinstall.js
@@ -5,37 +5,30 @@ var fs = require('fs-extra');
 
 var appDir = process.env.PWD;
 
-/*
- TODO
- //Config already exist so we don't override
- if (!fs.existsSync(appDir + "/../../config/passport.js"))
- {
- //Copy base acl config
- fs.copy(appDir + "/templates/passport.js", appDir + "/../../config/passport.js", function (err)
- {
- if (err)
- {
- console.log(err);
- }
- else
- {
- console.log("done write passport.js base config");
- }
- });
- }
- if (!fs.existsSync(appDir + "/../../views/auth"))
- {
- //Copy base acl config
- fs.copy(appDir + "/templates/auth", appDir + "/../../views/auth", function (err)
- {
- if (err)
- {
- console.log(err);
- }
- else
- {
- console.log("done write auth base views");
- }
- });
- }
- */
+var mergeDirs = function (f1, f2, overwriteExistingFiles) {
+
+  var file, files, stats, _i, _len;
+  debugger;
+  files = fs.readdirSync(f1);
+
+  for (_i = 0, _len = files.length; _i < _len; _i++) {
+    file = files[_i];
+    stats = fs.lstatSync("" + f1 + "/" + file);
+    console.log(stats + " " + f1 + "/" + file + "\n" + f2 + "/" + file);
+    if (stats.isDirectory()) {
+      mergeDirs("" + f1 + "/" + file, "" + f2 + "/" + file)
+    }
+    else {
+      if (!fs.existsSync("" + f2 + "/" + file) || overwriteExistingFiles) {
+        var path = ("" + f2 + "/" + file).split("/").slice(0, -1).join("/");
+        if (!fs.existsSync(path)) {
+          fs.mkdirSync(path, 0x1ed, true);
+        }
+        fs.writeFileSync("" + f2 + "/" + file, fs.readFileSync("" + f1 + "/" + file));
+      }
+    }
+  }
+};
+
+mergeDirs(__dirname + "/..views", __dirname + "/../../../views");
+mergeDirs(__dirname + "/..assets", __dirname + "/../../../assets");
